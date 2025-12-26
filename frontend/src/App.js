3d267243a1d958f06d0ac2d06d6a75e33b57f763
Ketan <ketan.darekar99@gmail.com>
2025-12-26 11:19:25 +0100

    Home Page Creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,75 @@
 import './App.css';
 
 
-let name = prompt("enter your name:");
 function App() {
   return (
     <>
-    <div className="blank">nice to meet you</div>
-    <header>
-      <nav>
-        <h1>home</h1>
-        <h1>about</h1>
-        <h1>contact</h1>
-      </nav>
-    </header>
-    <div className="container">My name is Ketan</div>
-    <h3>My name is {name}</h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aut consequuntur, accusamus omnis fugiat dicta repellendus dolorum nesciunt impedit suscipit consectetur, delectus voluptate similique quas nihil, eum autem? Soluta, rerum?
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav> 
+
+{/* Card component (placeholders)*/}
+<div className="card">
+  <img src="..." className="card-img-top" alt="..."/>
+
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="/" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div className="card" aria-hidden="true">
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title placeholder-glow">
+      <span className="placeholder col-6"></span>
+    </h5>
+    <p className="card-text placeholder-glow">
+      <span className="placeholder col-7"></span>
+      <span className="placeholder col-4"></span>
+      <span className="placeholder col-4"></span>
+      <span className="placeholder col-6"></span>
+      <span className="placeholder col-8"></span>
     </p>
+    <a href ="/" className="btn btn-primary disabled placeholder col-6" aria-disabled="true">Go to Main Screen</a>
+  </div>
+</div>
+
+{/* This is a footer */}
+<div className="card text-center">
+  <div className="card-header">
+    Featured
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Special title treatment</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="/" className="btn btn-primary">Go somewhere</a>
+  </div>
+  <div className="card-footer text-body-secondary">
+    2 days ago
+  </div>
+</div>
+
     </>
   );
 }
