5fee41aacaf1c86e9d0f01044a85400125610f24
Ketan <ketan.darekar99@gmail.com>
2026-01-08 11:23:04 +0100

    Dark Mode Feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-
-
+//import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     <Navbar title = "My React App" abouttext ="This is an professional App"/>
+    <About/>
     <div className="container">
-      <TextForm submit = "submit" heading = "Enter the text to analyze"/>
+     {/*<TextForm submit = "submit" heading = "Enter the text to analyze"/>*/}
     </div>
     
     </>
