b930d570652de43140621c9ac0568c5f88194478
Ketan <ketan.darekar99@gmail.com>
2026-01-20 09:45:10 +0100

    React Routing using Router Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import Navbar from './components/Navbar';
 //import Start from './components/Start';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-
+import Footer from './components/Footer';
+import About from './components/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not 
@@ -27,23 +29,34 @@ function App() {
       setMode ('dark');
       document.body.style.backgroundColor ="grey";
       showAlert("Dark mode has been enabled", "success");
+      document.title = "Text Utils - Dark Mode";
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode has been enabled", "success");
+      document.title = "Text Utils - Light Mode";
     }
     
   }
   return (
     <>
+    <BrowserRouter>
     <Navbar title = "My React App" abouttext ="This is an professional App" mode ={mode} toggleMode ={toggleMode}/>
     {/*<About/>*/}
     <Alert alert={alert}/>
     <div className="container">
-     <TextForm showAlert ={showAlert} submit = "submit" heading = "Enter the text to analyze"  mode = {mode}/>
+      <Routes>
+        <Route path ="/about" element ={<About/>}>
+        </Route>
+        <Route path ="/" element ={<TextForm showAlert ={showAlert} submit = "submit" heading = "Enter the text to analyze"  mode = {mode}/>}>
+        </Route>
+      </Routes>
      {/*<Start heading ="Hi I am the one"/>*/}
+     {/*<About/>*/}
+     <Footer/>
     </div>
+    </BrowserRouter>
     
     </>
   );
