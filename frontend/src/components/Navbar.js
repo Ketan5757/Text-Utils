44689c0dc413d1901da76d9214f7b6386ffa7fac
Ketan <ketan.darekar99@gmail.com>
2026-02-03 08:12:52 +0100

    Enable Togglebutton

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -22,10 +22,9 @@ export default function Navbar(props) {
           <Link className="nav-link" to="/about">{props.abouttext}</Link>
         </li>
       </ul>
-      {/*<form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>*/}
+      <div className="d-flex">
+        <div className="bg-primary rounded mx-2" onClick = {props.toggleMode} style ={{height:'30px', width: '30px'}}></div>
+      </div>
       <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
       <input className="form-check-input" onClick ={props.toggleMode}type="checkbox" role="switch" id="switchCheckDefault"/>
       <label className="form-check-label" htmlFor="switchCheckDefault">Enable Dark Mode</label>
