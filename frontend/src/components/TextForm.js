c12336ca2f972122df561c25ca43feb406b3890e
Ketan <ketan.darekar99@gmail.com>
2026-02-03 08:55:31 +0100

    new line text form issue solved

--- frontend/src/components/TextForm.js
+++ frontend/src/components/TextForm.js
@@ -60,7 +60,7 @@ export default function TextForm(props) {
   <button disabled = {text.length === 0} className="btn btn-primary mx-2" onClick ={clearText}>Clear Text</button>
   <div className="container my-3" style = {{backgroundColor: props.mode === 'light'?"white":"grey"}}>
     <h1>Your Text Summary</h1>
-    <p><b>{text.split(" ").filter((element)=>{return element.length!==0}).length}</b> words and <b>{text.length}</b> characters</p>
+    <p><b>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</b> words and <b>{text.length}</b> characters</p>
     <p><b>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length}</b> Minutes to read</p>
     <h2>Preview</h2>
     <p>{text.length >0 ? text: 'Nothing to preview!'}</p>
